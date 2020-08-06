4dc247f661a18a4f8dc36f63e4274bf28f0b3ddc
Chaitanya Samani <ch330019@wipro.com>
2020-08-06 17:15:19 +0530

    Added the hello endpoint

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Welcome to the home page!"
 
+@app.route("/hello")
+def hello():
+    return "Hello from openshift!!"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
